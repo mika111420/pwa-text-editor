e76a94d308afcd60f9ed66741d74b6402fb94b17
Mikayla <user@users-MBP.attlocal.net>
2023-09-07 18:22:16 -0400

    added css loaders and configured workbox plugins

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const path = require('path');
 
-app.use(express.static('../client/dist'));
+const PORT = process.env.PORT || 3005;
+
+app.use(express.static('../client'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
